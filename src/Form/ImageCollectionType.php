cb74996964e7080c5d1ab18cdf311ac53693eca6
BarthyB <barthy.bonhomme@gmail.com>
2020-04-07 16:37:18 +0200

    forgot collections

--- src/Form/ImageCollectionType.php
+++ src/Form/ImageCollectionType.php
@@ -37,22 +37,23 @@ class ImageCollectionType extends AbstractType
 
         $resolver->setDefaults(
             [
-                'accept'                => 'image/jpeg',
-                'cropper_aspect_width'  => null,
-                'cropper_aspect_height' => null,
-                'sortable'              => false,
-                'allow_add'             => true,
-                'allow_delete'          => true,
-                'translation_domain'    => 'admin',
-                'label'                 => 'article.images.label',
-                'prototype_placeholder' => '_images_',
-                'entry_type'            => function (Options $options) {
+                'accept'                      => 'image/jpeg',
+                'cropper_aspect_width'        => null,
+                'cropper_aspect_height'       => null,
+                'sortable'                    => false,
+                'allow_add'                   => true,
+                'allow_delete'                => true,
+                'translation_domain'          => 'admin',
+                'label'                       => 'article.images.label',
+                'excluded_translation_fields' => [],
+                'prototype_placeholder'       => '_images_',
+                'entry_type'                  => function (Options $options) {
                     return $options['sortable'] ? SortableImageUploadType::class : ImageUploadType::class;
                 },
-                'prototype_name'        => function (Options $options) {
+                'prototype_name'              => function (Options $options) {
                     return $options['prototype_placeholder'];
                 },
-                'attr'                  => function (Options $options) use ($prepend, $append, $add, $sort) {
+                'attr'                        => function (Options $options) use ($prepend, $append, $add, $sort) {
                     return [
                         'data-prototype-placeholder' => $options['prototype_placeholder'],
                         'data-allow-add'             => var_export($options['allow_add'], true),
@@ -63,12 +64,13 @@ class ImageCollectionType extends AbstractType
                         'data-sort-title'            => $sort,
                     ];
                 },
-                'entry_options'         => function (Options $options) {
+                'entry_options'               => function (Options $options) {
                     return [
-                        'label'                 => false,
-                        'accept'                => $options['accept'],
-                        'cropper_aspect_width'  => $options['cropper_aspect_width'],
-                        'cropper_aspect_height' => $options['cropper_aspect_height'],
+                        'label'                       => false,
+                        'accept'                      => $options['accept'],
+                        'cropper_aspect_width'        => $options['cropper_aspect_width'],
+                        'cropper_aspect_height'       => $options['cropper_aspect_height'],
+                        'excluded_translation_fields' => $options['excluded_translation_fields'],
                     ];
                 },
             ]
